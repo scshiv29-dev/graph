3af4f61056f959af87c21df9b3c090fde5c2a7e4
scshiv29-dev <shivchaudhary790@hotmail.com>
2021-03-02 07:24:07 +0530

    final

--- Graph.py
+++ Graph.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 
 
 class Graph:
+    # initializing the class with no of vertices
     def __init__(self, vertices):
+        # declaring labeldict for labeling of graph
         self.labeldict = {}
+        # initializing vertices
         self.V = vertices
+        # initializing graph with all values to 0
         self.graph = []
-        self.not_found=[]
         for i in range(self.V):
             self.labeldict[i] = i
             l = []
@@ -17,11 +20,12 @@ class Graph:
             self.graph.append(l)
 
     def add_edge(self, src, dst):
+        # adding path i.e 1 from src to dst
         self.graph[src][dst] = 1
 
-
+    # checking for simple path
     def check_simple_path(self, src, dst):
-        self.path.append(src)
+
         found = False
         if self.graph[src][dst] == 1:
             found = True
@@ -31,13 +35,12 @@ class Graph:
                 if self.graph[src][i] == 1:
                     temp.append(i)
 
-            # print(temp)
             if temp is not None:
                 for t in temp:
+                    # print(t)
+                    # print(temp)
                     if self.graph[t][dst] == 1:
                         found = True
-
-
                         break
                     else:
                         for i in range(self.V):
@@ -45,15 +48,17 @@ class Graph:
                                 temp.append(i)
 
         if found:
-            self.path.append(dst)
             print("There is a path")
 
         else:
             print("No path found")
 
-    def print_graph(self):
+    # printing matrix format of graph
 
+    def print_graph(self):
         print(np.matrix(self.graph))
+
+    # drawing graph using matplotlib and networkx
 
     def draw_graph(self):
         alg = nx.DiGraph
@@ -63,20 +68,19 @@ class Graph:
         plt.show()
 
 
-g=Graph(8)
-g.add_edge(0,3)
-g.add_edge(3,5)
-g.add_edge(5,6)
-g.add_edge(6,7)
-g.add_edge(3,4)
-g.add_edge(4,3)
-g.add_edge(1,0)
-g.add_edge(1,2)
-g.add_edge(1,4)
-g.add_edge(4,6)
-g.add_edge(2,7)
-g.add_edge(7,1)
-g.check_simple_path(0,1)
+g = Graph(8)
+g.add_edge(0, 3)
+g.add_edge(3, 5)
+g.add_edge(5, 6)
+g.add_edge(6, 7)
+g.add_edge(3, 4)
+g.add_edge(1, 0)
+g.add_edge(1, 2)
+g.add_edge(1, 4)
+g.add_edge(4, 6)
+g.add_edge(2, 7)
+g.add_edge(7, 1)
+g.check_simple_path(0, 1)
+g.check_simple_path(2, 6)
 g.print_graph()
 g.draw_graph()
-
